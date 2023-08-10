ded16c243e815d6cd0cb8bc0c965708a27382e68
Aleksandr <aleksandr.koshkin.2000@mail.ru>
2023-08-10 13:23:57 +0300

    One

---
+++ src/main/java/org/example/controller/AuthorizationController.java
@@ -0,0 +1,39 @@
+package org.example.controller;
+
+import org.example.exception.InvalidCredentials;
+import org.example.exception.UnauthorizedUser;
+import org.example.model.Authorities;
+import org.example.service.AuthorizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AuthorizationController {
+    AuthorizationService service;
+
+    private AuthorizationController() {
+        this.service = new AuthorizationService();
+    }
+
+    @GetMapping("/authorize")
+    private List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
+        return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    private ResponseEntity<String> handleInvalidCredentials(InvalidCredentials e) {
+        return new ResponseEntity<>("Exception: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    private ResponseEntity<String> handleUnauthorizedUser(UnauthorizedUser e) {
+        System.out.println("Exception: " + e.getMessage());
+        return new ResponseEntity<>("Exception: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
---
+++ src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package org.example.repository;
+
+import org.example.model.Authorities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    public List<Authorities> getUserAuthorities(String user, String password) {
+        List<Authorities> authorities = new ArrayList<>();
+        if (user.equals("admin") && password.equals("0123")) {
+            authorities.add(Authorities.DELETE);
+            authorities.add(Authorities.WRITE);
+            authorities.add(Authorities.READ);
+        } else if (user.equals("user") && password.equals("3210")) {
+            authorities.add(Authorities.READ);
+        }
+        return authorities;
+    }
+}
